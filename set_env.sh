eda3f54d1986d01b85c10ca12f21b6084210c066
cao van thang <caovthang@gmail.com>
2022-04-23 14:51:29 +0700

    intial commit

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-postgres.cqlepzc1gk4s.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgresDB
+export AWS_BUCKET=arn:aws:s3:::test-265316526852-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID="AKIAT3RQY5MCKKL4ZQPP"
+export AWS_SECRET_ACCESS_KEY="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"
